d7cc2b56747e5e7b9e45213faa333e7a56d290f5
Corey Ellis <cellis@successpartners.com>
2018-09-19 00:19:54 -0500

    Landing pg revision 1

--- assets/dist/theme-bundle.chunk.14.js
+++ assets/dist/theme-bundle.chunk.14.js
@@ -1,1 +1,60 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[14],{306:function(t,e,n){"use strict";e.a={email:function(t){return/^.+@.+\..+/.test(t)},password:function(t){return this.notEmpty(t)},notEmpty:function(t){return t.length>0}}},377:function(t,e,n){"use strict";n.r(e);var o=n(51),r=n(92),i=n(0),a=n.n(i),c=n(306);var u=function(t){function e(){return function(t,e){if(!(t instanceof e))throw new TypeError("Cannot call a class as a function")}(this,e),function(t,e){if(!t)throw new ReferenceError("this hasn't been initialised - super() hasn't been called");return!e||"object"!=typeof e&&"function"!=typeof e?t:e}(this,t.apply(this,arguments))}return function(t,e){if("function"!=typeof e&&null!==e)throw new TypeError("Super expression must either be null or a function, not "+typeof e);t.prototype=Object.create(e&&e.prototype,{constructor:{value:t,enumerable:!1,writable:!0,configurable:!0}}),e&&(Object.setPrototypeOf?Object.setPrototypeOf(t,e):t.__proto__=e)}(e,t),e.prototype.onReady=function(){this.registerContactFormValidation()},e.prototype.registerContactFormValidation=function(){var t="form[data-contact-form]",e=Object(r.a)({submit:t+' input[type="submit"]'}),n=a()(t);e.add([{selector:t+' input[name="contact_email"]',validate:function(t,e){t(c.a.email(e))},errorMessage:this.context.contactEmail},{selector:t+' textarea[name="contact_question"]',validate:function(t,e){t(c.a.notEmpty(e))},errorMessage:this.context.contactQuestion}]),n.on("submit",function(t){e.performCheck(),e.areAll("valid")||t.preventDefault()})},e}(o.a);e.default=u}}]);
+(window["webpackJsonp"] = window["webpackJsonp"] || []).push([[14],{
+
+/***/ "./assets/js/theme/compare.js":
+/*!************************************!*\
+  !*** ./assets/js/theme/compare.js ***!
+  \************************************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _page_manager__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./page-manager */ "./assets/js/theme/page-manager.js");
+/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
+/* harmony import */ var jquery__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(jquery__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var sweetalert2__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! sweetalert2 */ "./node_modules/sweetalert2/dist/sweetalert2.min.js");
+/* harmony import */ var sweetalert2__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(sweetalert2__WEBPACK_IMPORTED_MODULE_2__);
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+
+
+
+
+var Compare = function (_PageManager) {
+    _inherits(Compare, _PageManager);
+
+    function Compare() {
+        _classCallCheck(this, Compare);
+
+        return _possibleConstructorReturn(this, _PageManager.apply(this, arguments));
+    }
+
+    Compare.prototype.onReady = function onReady() {
+        var _this2 = this;
+
+        var message = this.context.compareRemoveMessage;
+
+        jquery__WEBPACK_IMPORTED_MODULE_1___default()('body').on('click', '[data-comparison-remove]', function (event) {
+            if (_this2.context.comparisons.length <= 2) {
+                sweetalert2__WEBPACK_IMPORTED_MODULE_2___default()({
+                    text: message,
+                    type: 'error'
+                });
+                event.preventDefault();
+            }
+        });
+    };
+
+    return Compare;
+}(_page_manager__WEBPACK_IMPORTED_MODULE_0__["default"]);
+
+/* harmony default export */ __webpack_exports__["default"] = (Compare);
+
+/***/ })
+
+}]);
+//# sourceMappingURL=theme-bundle.chunk.14.js.map
